78e0b250b3fba2f59e01a3fcfdb59799f418ccaf
Iurii Kleimenov <yurakleimenovv@gmail.com>
2023-09-04 09:05:14 +0300

    Border color logic

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -10,6 +10,19 @@ final class MovieQuizViewController: UIViewController {
     @IBOutlet private var counterLabel: UILabel!
     @IBOutlet private var textLabel: UILabel!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let currentQuestion = questions[currentQuestionIndex]
+        let convertedQuestion = convert(model: currentQuestion)
+        show(quiz: convertedQuestion)
+        
+        imageView.layer.masksToBounds = true
+        imageView.layer.borderWidth = 8
+        imageView.layer.borderColor = UIColor.white.cgColor
+        imageView.layer.cornerRadius = 8 
+        
+    }
+    
     // models:
     struct QuizStepViewModel {
         let image: UIImage
@@ -72,13 +85,13 @@ final class MovieQuizViewController: UIViewController {
             text: "Рейтинг этого фильма больше чем 6?",
             correctAnswer: false)
     ]
-
+    
     // variables for counting questions and correct answers
     private var currentQuestionIndex = 0
     private var correctAnswers = 0
     
     
-    
+    // method that converts questions to View
     private func convert(model: QuizQuestion) -> QuizStepViewModel {
         let questionStep = QuizStepViewModel(
             image: UIImage(named: model.image) ?? UIImage(),
@@ -87,32 +100,33 @@ final class MovieQuizViewController: UIViewController {
         return questionStep
     }
     
+    // method that shows question view
     private func show(quiz step: QuizStepViewModel) {
         imageView.image = step.image
         textLabel.text = step.question
         counterLabel.text = step.questionNumber
     }
     
+    // method that changes border color
+    private func showAnswerResult(isCorrect: Bool) {
+        imageView.layer.masksToBounds = true
+        imageView.layer.borderWidth = 8
+        imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
+    }
+    
     @IBAction private func noButtonClicked(_ sender: Any) {
+        let currentQuestion = questions[currentQuestionIndex]
+        let givenAnswer = false
         
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
     }
     @IBAction private func yesButtonClicked(_ sender: Any) {
-        
-    }
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
         let currentQuestion = questions[currentQuestionIndex]
-        let convertedQuestion = convert(model: currentQuestion)
-        show(quiz: convertedQuestion)
+        let givenAnswer = true
         
-        
-        yesButton.titleLabel!.font = UIFont(name: "YSDisplay-Medium", size: 20)
-        noButton.titleLabel!.font = UIFont(name: "YSDisplay-Medium", size: 20)
-        textLabel.font = UIFont(name: "YSDisplay-Bold", size: 23)
-        counterLabel.font = UIFont(name: "YSDisplay-Medium", size: 20)
+        showAnswerResult(isCorrect: givenAnswer == currentQuestion.correctAnswer)
     }
+    
 }
 
 
